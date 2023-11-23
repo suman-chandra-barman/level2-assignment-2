ac409d4faa0630dc6bf3ce2bd5cac77d0bfc2ba5
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-11-23 19:51:41 +0600

    Prevented password field in response data

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,14 +2,32 @@ import { TUser } from './user.interface';
 import { User } from './user.model';
 
 const createUserIntoDB = async (user: TUser) => {
-  const result = await User.create(user);
-  return result;
+  const newUser = await User.create(user);
+  const { password, ...withOutPassword } = newUser.toJSON();
+  return withOutPassword;
 };
 const getAllUsersFromBD = async () => {
-  const result = await User.find();
-  return result;
+  const users = await User.find();
+
+  const formattedUsers = users.map((user) => ({
+    username: user.username,
+    fullName: user.fullName,
+    age: user.age,
+    email: user.email,
+    address: user.address,
+  }));
+  return formattedUsers;
 };
 const getSingleUserFromBD = async (id: string) => {
+  if (await User.isUserExists(id)) {
+    const result = await User.findOne({ userId: id });
+    const { password, ...withOutPassword } = result?.toJSON();
+    return withOutPassword;
+  } else {
+    throw new Error();
+  }
+};
+const updateUserFromDB = async (id: string) => {
   if (await User.isUserExists(id)) {
     const result = await User.findOne({ userId: id });
     return result;
@@ -22,4 +40,5 @@ export const UserServices = {
   createUserIntoDB,
   getAllUsersFromBD,
   getSingleUserFromBD,
+  updateUserFromDB,
 };
